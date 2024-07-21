279e00f92286162d6e9f74be0ef344877c2091e4
Fantom <officialfantomwrld@gmail.com>
2024-07-21 10:53:33 +0200

    Added drawing functionality + changable grid size

--- main.js
+++ main.js
@@ -1,23 +1,47 @@
 const container = document.querySelector('.container');
+const changeGridButton = document.querySelector('#change-grid-size');
+
+let gridSize = 16;
+
+makeGrid();
+
+changeGridButton.addEventListener('click', () => {
+    gridSize = prompt('What would you like the grid size to be? It must be between 0 and 100.');
+
+    while(true) {
+        if(gridSize > 0 && gridSize <= 100) {
+            removeGrid();
+            makeGrid();
+            break;
+        } else {
+            gridSize = prompt('Invalid Grid Size!\nWhat would you like the grid size to be? It must be between 0 and 100.');
+        }
+    }
+});
 
 function makeGrid() {
-    for(i = 0; i < 16; i++) {
+    for(i = 0; i < gridSize; i++) {
         const row = document.createElement("div");
         row.classList.add('row');
         container.appendChild(row);
 
-        console.log(row);
-
-        for(j = 0; j < 16; j++) {
+        for(j = 0; j < gridSize; j++) {
             const block = document.createElement("div");
             block.classList.add('block');
             row.appendChild(block);
 
             block.addEventListener('mouseover', () => {
-                block.style.backgroundColor = '#414141';
-            })
+                let randomRed = Math.floor(Math.random() * 250) + 1;
+                let randomGreen = Math.floor(Math.random() * 250) + 1;
+                let randomBlue = Math.floor(Math.random() * 250) + 1;
+
+                block.style.backgroundColor = `rgb(${randomRed}, ${randomGreen}, ${randomBlue})`;
+            });
+
         }
     }
 }
 
-makeGrid();
+function removeGrid() {
+    container.replaceChildren();
+}
